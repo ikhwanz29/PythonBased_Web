97e8701bfb7ebac8402fd07153f22440167be529
ikhwanz29 <ikhwanuluzlahtkj@gmail.com>
2024-07-12 18:11:47 +0800

    Blood Donor Identification (Django)

--- proyek_donor_darah/settings.py
+++ proyek_donor_darah/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i&f&2@&k8n_zvphl&ntku9h1))_dwqi(ne43n9+o9j@*!5=^1%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 CSRF_TRUSTED_ORIGINS = []  
 
 # Application definition
